b0f68a775a71dd3118022e9851e9358a2937a28d
David Adler <d.adler.s@gmail.com>
2011-09-06 19:30:05 -0400

    Updated Sorted module: changes and new functions.
    Changed union function to act like Data.List.union. Added slight
    optimization to nub. Added difference, which acts like (\\). Added elem,
    which acts like Data.List.elem, and also mapElem, which efficiently
    implements (map elem) on two sorted lists.

--- Sorted.hs
+++ Sorted.hs
@@ -32,18 +32,27 @@ mergeManyStable xs = head $ until single mm xs
       mm [z] = [z]
       mm (x:y:zs) = merge x y : mm zs
 
-union [] yy = yy
+union [] yy = nub yy
 union xx [] = xx
 union xx@(x:xs) yy@(y:ys)
-    | y < x = y : union xx ys
+    | y < x = union (y:xx) ys
     | x < y = x : union xs yy
     | otherwise = union xx ys
 
+difference [] _ = []
+difference xx [] = xx
+difference xx@(x:xs) yy@(y:ys)
+    | y < x = difference xx ys
+    | x < y = x : difference xs yy
+    | otherwise = difference xs ys
+
 nub [] = []
-nub [x] = [x]
-nub (x:xs@(y:ys))
-    | x == y = nub (x:ys)
-    | otherwise = x : nub xs
+nub (x:xs) = nub' x xs
+    where
+      nub' x (y:ys)
+          | x == y = nub' x ys
+          | otherwise = x : nub' y ys
+      nub' x [] = [x]
 
 count [] = []
 count (x:xs) = count' x 1 xs
@@ -58,3 +67,16 @@ uncount ((i,c):rest)
     | c == 1 = i : uncount rest
     | c > 1 = i : uncount ((i,c-1):rest)
     | c == 0 = uncount rest
+
+elem x [] = False
+elem x (y:ys)
+    | x > y = x `Sorted.elem` ys
+    | otherwise = (x == y)
+
+mapElem :: (Ord a) => [a] -> [a] -> [Bool]
+mapElem [] _ = []
+mapElem xs [] = map (const False) xs
+mapElem xx@(x:xs) yy@(y:ys) = case x `compare` y of
+                          LT -> False : mapElem xs yy
+                          GT -> mapElem xx ys
+                          EQ -> True : mapElem xs yy
