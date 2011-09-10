69aa67a77a92660fafa21328c9b737e56aabb6ab
David Adler <d.adler.s@gmail.com>
2011-09-09 20:13:28 -0400

    Added problems 33, 34, and 35.

--- Set3.hs
+++ Set3.hs
@@ -1,10 +1,14 @@
 module Set3 (set3) where
 
-import EulerUtil (digits,undigits,selectAntiSelect)
-import Data.List (sort)
-import Sorted (nub)
+import EulerUtil (digits,undigits,selectAntiSelect,allBut)
+import Data.List (sort,inits,tails)
+import Sorted (nub,mapElem)
+import Data.Ratio
+import Data.Maybe (mapMaybe)
+import Atkin (primes)
+import Data.Array.Unboxed (listArray,UArray,(!))
 
-set3 = take 10 $ [euler30,euler31,euler32] ++ repeat undefined
+set3 = take 10 $ [euler30,euler31,euler32,euler33,euler34,euler35] ++ repeat undefined
 
 euler30 = show . sum . filter (\x -> x == (sum . map (^5) . digits $ x)) $ [2..200000]
 
@@ -25,3 +29,32 @@ euler32 = show . sum . Sorted.nub . sort . concatMap valid . enumeratePnr 5 $ [1
       enumeratePnr c xs = (iterate (concatMap step) [([],xs)]) !! c
           where
             step (p,rem) = map (\(s,as) -> (s:p,as)) . selectAntiSelect $ rem
+
+euler33 = show . denominator . product . mapMaybe valid . concat
+          $ [typeA,typeB,typeC,typeD]
+    where
+      typeA = [([a,x],[b,x],a,b) | a <- [1..9], b <- [1..9], x <- [1..9]]
+      typeB = [([x,a],[b,x],a,b) | a <- [0..9], b <- [1..9], x <- [1..9]]
+      typeC = [([a,x],[x,b],a,b) | a <- [1..9], b <- [1..9], x <- [1..9]]
+      typeD = [([x,a],[x,b],a,b) | a <- [0..9], b <- [1..9], x <- [1..9]]
+      valid (ns,ds,n',d')
+          | (n < d) && (n % d) == val = Just val
+          | otherwise = Nothing
+          where
+            n = undigits ns
+            d = undigits ds
+            val = n'%d'
+
+euler34 = show . sum . filter (\x -> x == (sum . map fact . digits $ x))
+          $ [3..50000]
+    where
+      fact n = product [2..n]
+
+euler35 = primeMap `seq` show . length . filter circularPrime . takeWhile (< 1000000)
+          $ primes
+    where
+      circles xs = tail $ zipWith (++) (tails xs) (inits xs)
+      numCircles = map undigits . circles . digits
+      circularPrime = all (primeMap !) . sort . allBut 1 . numCircles
+      primeMap :: UArray Int Bool
+      primeMap = listArray (1,999999) $ [1..999999] `Sorted.mapElem` primes
