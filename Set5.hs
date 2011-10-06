d0897f5921c70cb191ed74c2fed2b993df4daca6
David Adler <d.adler.s@gmail.com>
2011-10-06 19:31:32 -0400

    Added problem 51.

--- Set5.hs
+++ Set5.hs
@@ -2,10 +2,11 @@ module Set5 (set5) where
 
 import Sorted (elem,mergeInfinite)
 import Atkin (primes)
-import Data.List (sortBy,tails)
+import Data.List (sortBy,tails,unfoldr,foldl',groupBy,sort)
 import Data.Ord (comparing)
+import EulerUtil (digits)
 
-set5 = take 10 $ [euler50] ++ repeat undefined
+set5 = take 10 $ [euler50,euler51] ++ repeat undefined
 
 euler50 = show . fst . head . filter (flip Sorted.elem primes . fst)
           . sortBy (flip $ comparing snd) . takeWhile ((< 1000000) . fst)
@@ -14,3 +15,29 @@ euler50 = show . fst . head . filter (flip Sorted.elem primes . fst)
       primeSpanSums = mergeInfinite
                       . map (\(h:t) -> flip zip [1..] . map (subtract h) $ t)
                       .  tails . scanl (+) 0 $ primes
+
+euler51 = show . head . concatMap selByG8 . bylog10 $ primes
+    where
+      selByG8 = sort . map (snd . head) . filter ((8 ==) . length)
+                . groupBy (\a b -> fst a == fst b) . sortBy (comparing fst)
+                . concatMap classifyPrime
+      bylog10 xs = unfoldr step (10,xs)
+          where
+            step (_,[]) = Nothing
+            step (u,xs) = Just (s1, (10*u,s2))
+                where
+                  (s1,s2) = span (< u) xs
+      classifyPrime p = filter (\((_,b),_) -> b /= 0)
+                        . map (\(a,b,_) -> (((foldl' b10 0 a)*10+o, foldr b2 0 b),p))
+                        $ trips
+          where
+            trips = foldr concatMap [([],[],Nothing)] . map step . digits $ p'
+            (p', o) = p `quotRem` 10
+            step d (a,b,mc) = (d:a,False:b,mc) : (step' d a b mc)
+            step' d a b Nothing = [(a,True:b,Just d)]
+            step' d a b jc@(Just c)
+                | d == c = [(a,True:b,jc)]
+                | otherwise = []
+            b10 r d = r*10+d
+            b2 True r = r*2+1
+            b2 False r = r*2
