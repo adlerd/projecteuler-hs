10b8dfa1f2b4b3809a3b02397267b92e847a8089
David Adler <d.adler.s@gmail.com>
2011-12-22 17:55:58 -0500

    Explicitly import sets into Main.

--- euler.hs
+++ euler.hs
@@ -1,12 +1,12 @@
 module Main where
 
-import Set0
-import Set1
-import Set2
-import Set3
-import Set4
-import Set5
-import Set6
+import Set0 (set0)
+import Set1 (set1)
+import Set2 (set2)
+import Set3 (set3)
+import Set4 (set4)
+import Set5 (set5)
+import Set6 (set6)
 
 lastProblem = 66
 sets = [set0,set1,set2,set3,set4,set5,set6]
