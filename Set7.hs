e325627347ad2abd4ad029c28a504cef656db3da
David Adler <d.adler.s@gmail.com>
2011-12-27 03:13:51 -0500

    Added problem 71.

--- Set7.hs
+++ Set7.hs
@@ -3,9 +3,10 @@ module Set7 (set7) where
 import Data.List (minimumBy,sort,foldl')
 import EulerUtil (totient,digits)
 import Data.Ord (comparing)
-import Data.Maybe (fromJust)
+import Data.Maybe (fromJust, mapMaybe)
+import Data.Ratio ((%))
 
-set7 = take 10 $ [euler70] ++ repeat undefined
+set7 = take 10 $ [euler70,euler71] ++ repeat undefined
 
 euler70 = show . fst . fromJust $ foldl' fold Nothing [2..9999999]
     where
@@ -17,3 +18,16 @@ euler70 = show . fst . fromJust $ foldl' fold Nothing [2..9999999]
             valid = (sort . digits $ x) == (sort . digits $ t)
             t = totient x
             ratio = fromIntegral x / fromIntegral t
+
+euler71 = show . fst . last . takeWhile ((<= 1000000) . snd) . iterate closer $ (2,7)
+    where
+      closer (n,d) = head . filter ((> r) . uncurry (%)) . mapMaybe (closest (n+1))
+                     $ [d+1..]
+          where
+            r = n%d
+      closest n d = fmap (\n -> (n,d)) . maybeLast . filter gcdone .
+                    takeWhile ((< 3%7) . (%d)) $ [n..]
+          where
+            maybeLast [] = Nothing
+            maybeLast xs = Just $ last xs
+            gcdone = (1 ==) . gcd d
