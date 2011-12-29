18f5f0fb310815c6f04f93d3061694f677e6f708
David Adler <d.adler.s@gmail.com>
2011-12-29 18:45:28 -0500

    Added problem 74.

--- Set7.hs
+++ Set7.hs
@@ -6,8 +6,9 @@ import EulerUtil (totient,digits)
 import Data.Ord (comparing)
 import Data.Maybe (fromJust, mapMaybe)
 import Data.Ratio ((%),denominator)
+import qualified Data.MemoCombinators as MC
 
-set7 = take 10 $ [euler70,euler71,euler72,euler73] ++ repeat undefined
+set7 = take 10 $ [euler70,euler71,euler72,euler73,euler74] ++ repeat undefined
 
 euler70 = show . fst . fromJust $ foldl' fold Nothing [2..9999999]
     where
@@ -38,3 +39,20 @@ euler72 = show . sum . map totient $ [2..1000000]
 euler73 = show  . sum . map (\d -> length . filter ((d==) . denominator)
                                    . takeWhile (< 1%2) . dropWhile (<= 1%3)
                                    . map (%d) $ [1..d]) $ [5..12000]
+
+euler74 = show . length . filter ((== 60) . chainLen') $ [1..999999]
+    where
+      fact = MC.arrayRange (0,9) (\d -> product [2..d])
+      chainLen _ 169 = 3
+      chainLen _ 363601 = 3
+      chainLen _ 1454 = 3
+      chainLen _ 871 = 2
+      chainLen _ 45361 = 2
+      chainLen _ 872 = 2
+      chainLen _ 45362 = 2
+      chainLen f x
+          | fdsum == x = 1
+          | otherwise = (1 +) . f . sum . map fact . digits $ x
+          where
+            fdsum = sum . map fact . digits $ x
+      chainLen' = MC.arrayRange (1,999999) (chainLen chainLen')
